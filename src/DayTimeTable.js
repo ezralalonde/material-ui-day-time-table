ede55e38bbe4c7eb2f635cd31d9325d6138b03fe
Ezra <ezra@usefuliftrue.com>
2017-11-23 15:09:34 -0600

    bump deps

--- src/DayTimeTable.js
+++ src/DayTimeTable.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Component, PropTypes } from "react"
+import { Component } from "react"
+import PropTypes from "prop-types"
 
 import { Table } from "material-ui/Table"
 import { TableHeader } from "material-ui/Table"
